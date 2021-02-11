886da7dccc3aa1a3baeadcbc6691703154c48f80
Balapashka <max.boostvink@gmail.com>
2021-02-11 11:43:09 +0600

    'released_functions'

--- app/src/main/java/com/practice_calculator/mycalculator/MainActivity.java
+++ app/src/main/java/com/practice_calculator/mycalculator/MainActivity.java
@@ -9,8 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    double in1 = 0, i2 = 0;
     TextView edittext1;
+    boolean Add, Sub, Multiply, Divide, Remainder, deci;
     Button button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_Add, button_Sub,
             button_Mul, button_Div, button_Equ, button_Del, button_Dot, button_Remainder;
 
@@ -110,5 +111,121 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Add = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Sub = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Multiply = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Divide = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Remainder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Remainder = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Equ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Add || Sub || Multiply || Divide || Remainder) {
+                    i2 = Float.parseFloat(edittext1.getText() + "");
+                }
+
+                if (Add) {
+
+                    edittext1.setText(in1 + i2 + "");
+                    Add = false;
+                }
+
+                if (Sub) {
+
+                    edittext1.setText(in1 - i2 + "");
+                    Sub = false;
+                }
+
+                if (Multiply) {
+                    edittext1.setText(in1 * i2 + "");
+                    Multiply = false;
+                }
+
+                if (Divide) {
+                    edittext1.setText(in1 / i2 + "");
+                    Divide = false;
+                }
+                if (Remainder) {
+                    edittext1.setText(in1 % i2 + "");
+                    Remainder = false;
+                }
+            }
+        });
+
+        button_Del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+                in1 = 0.0;
+                i2 = 0.0;
+            }
+        });
+
+        button_Dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (deci) {
+                    //do nothing or you can show the error
+                } else {
+                    edittext1.setText(edittext1.getText() + ".");
+                    deci = true;
+                }
+
+            }
+        });
     }
 }
